48224475d2833528282ef3fa5ce81ebd8993aea7
Aniket Bhardwaj <anicse37@gmail.com>
2025-06-27 16:26:07 +0530

    Revert "added env file"
    This reverts commit 12d42aae3bc58c4a11211337429ea2b80f0878b0.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	router "github.com/anicse37/Library_Management/Router"
 	"github.com/anicse37/Library_Management/internal/models"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	dns := os.Getenv("DNS")
+const dns = "go_user:S3cur3P@ssw0rd@tcp(127.0.0.1:3306)/library_db?charset=utf8mb4&parseTime=true&loc=Local"
 
+func main() {
 	SuperAdmin := models.User{
 		Id:       "MAU21UCS014",
 		Name:     "Super Admin",
--- go.mod
+++ go.mod
@@ -13,5 +13,4 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 )
