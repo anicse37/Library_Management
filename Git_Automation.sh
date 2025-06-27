7ff7909e2719a6fb5b081d7da268ecac69ec9976
Aniket Bhardwaj <anicse37@gmail.com>
2025-06-27 13:15:35 +0530

    Modefied Git_Automation.sh

--- Git_Automation.sh
+++ Git_Automation.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "Hey Lazzzzzy"
+
+
+# This script is used just to push code to 
+# github.com/anicse37/Library_Management
+# and does not serve any purpose in this software.
+echo "Hey!!!"
 
 git status 
 
 read  -p "Enter yout commit: " msg
 
 git add .
+
 git status
 
-
-
 git commit -m "$msg"
-
-
 
 git push
